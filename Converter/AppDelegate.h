7d389efe4c122e3fc49f3501f64e69afc3aa8ddd
vladimir.avstriychenko <vladimir.avstriychenko@dcsportsoft.com>
2024-02-28 15:46:59 +0300

    Initial Commit

---
+++ Converter/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Converter
+//
+//  Created by Vladimir Avstriychenko on 28.02.2024.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Converter/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  Converter
+//
+//  Created by Vladimir Avstriychenko on 28.02.2024.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
